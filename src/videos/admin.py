17da1b1ae7184613ef0a5c2201063fb79a1c43f8
Subham Singh <subham.singh@hackerearth.com>
2021-04-21 00:10:57 +0530

    video: add some more fields in video model

--- src/videos/admin.py
+++ src/videos/admin.py
@@ -1,26 +1,32 @@
 from django.contrib import admin
 from videos.models import VideoAllProxy, VideoPublishedProxy
+
 # Register your models here.
 
 
 class VideoAllAdmin(admin.ModelAdmin):
-    list_display = ['title', 'video_id']
-    search_fields = ['title']
-    #list_filter = ['video_id']
+    list_display = ["title", "video_id", "is_published", "state"]
+    search_fields = ["title"]
+    list_filter = ["active", "state"]
+    readonly_fields = ["id", "is_published", "publish_timestamp"]
 
     class Meta:
         model = VideoAllProxy
 
+    # def published(self, obj, *args, **kwargs):
+    #     return obj.active
+
 
 class VideoPublishedProxyAdmin(admin.ModelAdmin):
-    list_display = ['title', 'video_id']
-    search_fields = ['title']
-    
+    list_display = ["title", "video_id"]
+    search_fields = ["title"]
+
     class Meta:
         model = VideoPublishedProxy
 
     def get_queryset(self, request):
         return VideoAllProxy.objects.filter(active=True)
+
 
 admin.site.register(VideoAllProxy, VideoAllAdmin)
 admin.site.register(VideoPublishedProxy, VideoPublishedProxyAdmin)
--- src/videos/models.py
+++ src/videos/models.py
@@ -1,29 +1,64 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
 class Video(models.Model):
+    class VideoStateOptions(models.TextChoices):
+        # CONSTant = DB_VALUE, USER_DISPLAY_NAME
+        PUBLISH = "PU", "Publish"
+        DRAFT = "DR", "Draft"
+        # UNLISTED = 'UN', 'Unlisted',
+        # PRIVATE = 'PR', 'Private'
+
     title = models.CharField(max_length=220)
+
     description = models.TextField(blank=True, null=True)
+
     slug = models.SlugField(blank=True, null=True)
+
     video_id = models.CharField(max_length=220)
+
     active = models.BooleanField(default=True)
-    # timestamp = models.DateTimeField
     # updated = models.DateTimeField
-    # state
-    # publish_timestamp
+    state = models.CharField(
+        max_length=2, choices=VideoStateOptions.choices, default=VideoStateOptions.DRAFT
+    )
+
+    publish_timestamp = models.DateTimeField(
+        auto_now_add=False, auto_now=False, blank=True, null=True
+    )
 
     def __str__(self):
         return self.title
 
+    def timestamp(self):
+        return None
+
+    @property
+    def is_published(self):
+        return self.active
+
+    def save(self, *args, **kwargs):
+        if (
+            self.state == self.VideoStateOptions.PUBLISH
+            and self.publish_timestamp is None
+        ):
+            print("save as timestamp for published")
+            self.publish_timestamp = timezone.now()
+        elif self.state == self.VideoStateOptions.DRAFT:
+            self.publish_timestamp = None
+        super().save(*args, **kwargs)
+
+
 class VideoAllProxy(Video):
     class Meta:
         proxy = True
-        verbose_name = 'All Video'
-        verbose_name_plural = 'All Videos'
+        verbose_name = "All Video"
+        verbose_name_plural = "All Videos"
 
 
 class VideoPublishedProxy(Video):
     class Meta:
         proxy = True
-        verbose_name = 'Published Video'
-        verbose_name_plural = 'Published Videos'
+        verbose_name = "Published Video"
+        verbose_name_plural = "Published Videos"
